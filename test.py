67330f68afb9a57df4865f2fb32c8c2c145bd2c6
eggyford <eddieford854@gmail.com>
2025-05-03 02:11:58 -0400

    Added a range adjustment func

--- test.py
+++ test.py
@@ -2,26 +2,37 @@ import pip._vendor.requests as requests
 import time
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
-devices = AudioUtilities.GetSpeakers()
-interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
-volume = interface.QueryInterface(IAudioEndpointVolume)
 
-APIKEY = ""
 
-url = "https://dev.pulsoid.net/api/v1/data/heart_rate/latest?response_mode=text_plain_only_heart_rate"
-headers = {
-    "Authorization": "Bearer " + APIKEY,
-    "Content-Type": "application/json"
-}
+def main():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    volume = interface.QueryInterface(IAudioEndpointVolume)
 
-while(True):
-    response = requests.get(url, headers=headers)
-    print("Status Code:", response.status_code)
-    print("HR: ", response.text)
-    heartrate = int(response.text)
+    APIKEY = "1adce522-edca-48ba-b4e1-212552bca6eb"
 
-    computerVolume = max((heartrate-60)/2, 10)/100
-    volume.SetMasterVolumeLevelScalar(computerVolume, None)
-    print("Volume: ", round(volume.GetMasterVolumeLevelScalar() * 100))
+    url = "https://dev.pulsoid.net/api/v1/data/heart_rate/latest?response_mode=text_plain_only_heart_rate"
+    headers = {
+        "Authorization": "Bearer " + APIKEY,
+        "Content-Type": "application/json"
+    }
 
-    time.sleep(5)
+    while(True):
+        response = requests.get(url, headers=headers)
+        print("Status Code:", response.status_code)
+        print("HR: ", response.text)
+        heartrate = int(response.text)
+
+        computerVolume = max(rangeAdjust(60,200,heartrate),10)/100
+        volume.SetMasterVolumeLevelScalar(computerVolume, None)
+        print("Volume: ", round(volume.GetMasterVolumeLevelScalar() * 100))
+
+        time.sleep(5)
+
+def rangeAdjust(min: int, max: int, val: int):
+    OldRange = (max - min)  
+    NewRange = (100 - 0)
+    NewValue = (((val - min) * NewRange) / OldRange)
+    return NewValue
+
+main()
